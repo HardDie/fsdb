a7b6ea5775886c7ced1aab3abf90fdad6fd9a4e9
HardDie <oleg1995sysoev@yandex.ru>
2024-03-08 01:52:11 +0300

    Updated fuzz tests

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func FuzzNameToID(f *testing.F) {
+func FuzzNameToIDFile(f *testing.F) {
 	// Check before starting fuzzing
 	tmp, err := os.MkdirTemp("", "fsentry")
 	if err != nil {
@@ -55,6 +55,55 @@ func FuzzNameToID(f *testing.F) {
 		err = os.Remove(filePath)
 		if err != nil {
 			t.Fatalf("error removing file %q: %s", resName, err.Error())
+		}
+	})
+}
+
+func FuzzNameToIDFolder(f *testing.F) {
+	// Check before starting fuzzing
+	tmp, err := os.MkdirTemp("", "fsentry")
+	if err != nil {
+		f.Fatal("error creating temp dir", err)
+	}
+	err = os.Remove(tmp)
+	if err != nil {
+		f.Fatal("error removing temp dir", err)
+	}
+
+	tests := []string{
+		"con",
+		"prn",
+		"aux",
+		"nul",
+		"com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9", "com0",
+		"lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9", "lpt0",
+	}
+	for _, tc := range tests {
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, name string) {
+		resName := NameToID(name)
+		if resName == "" {
+			t.Logf("Empty name: %q", name)
+			return
+		}
+
+		tmpDirName, err := os.MkdirTemp("", "fsentry")
+		if err != nil {
+			f.Fatal("error creating temp dir", err)
+		}
+		defer os.RemoveAll(tmpDirName)
+
+		filePath := path.Join(tmpDirName, resName)
+
+		err = os.Mkdir(filePath, 0755)
+		if err != nil {
+			t.Fatalf("error create folder %q: %s", resName, err.Error())
+		}
+		err = os.Remove(filePath)
+		if err != nil {
+			t.Fatalf("error removing folder %q: %s", resName, err.Error())
 		}
 	})
 }
